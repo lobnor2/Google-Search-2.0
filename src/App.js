2945182b542c608b330c7c70ae9f9c89ca656641
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-01-06 14:47:15 +0530

    added few styles for navbar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
   const [darkTheme, setDarkTheme] = useState(false);
   return (
     <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-gray-200">Hellow World</div>
+      <div className="bg-gray-200 dark:bg-gray-900 dark:text-gray-100 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ReactPlayer from "react-player";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({ setDarkTheme, darkTheme }) => {
+  return (
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-300">
+      <div className="flex justify-between items-center space-x-5 w-screen">
+        <Link to="/">
+          <p className="text-2xl  text-white bg-blue-500 px-2 py-1 rounded dark:by-gray-500 dark:text-gray-900">
+            Google
+          </p>
+        </Link>
+        <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
+          {darkTheme ? "Light" : "Dark"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
