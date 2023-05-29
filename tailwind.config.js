2cdc2ecc1cd3b8ecaa417d78c83e41191b63694f
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-30 01:00:16 +0530

    Add a customize css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        c: "0 1px 6px rgb(32 33 36 / 28%)",
+        c2: "0 1px 1px rbg(0 0 0 / 10%)",
+      },
+    },
   },
   plugins: [],
 };
