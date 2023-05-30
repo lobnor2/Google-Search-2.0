b4effb829e2e0e286a8ae68de25ef9581db3fed9
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-30 23:40:53 +0530

    Add a home header, main and footer component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Logo from "../assets/google-logo.png";
+import HomeHeader from "./HomeHeader";
+import SearchInput from "./SearchInput";
+import Footer from "./Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex h-[100vh] flex-col  border border-red-600">
+      <HomeHeader />
+      {/* main section */}
+      <main className="grow flex justify-center border border-blue-800 my-2">
+        <div className="w-full px-5 flex flex-col items-center mt-44 border border-green-700">
+          <img
+            src={Logo}
+            alt=""
+            className="border border-black w-[172px] md:w-[272px] mb-8"
+          />
+          <SearchInput />
+          <div className="flex gap-2 text-[#3c4043] mt-8">
+            <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">
+              Google Search
+            </button>
+            <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">
+              I'm Feeling Lucky
+            </button>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
