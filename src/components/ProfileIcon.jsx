f6e964827888c69a166dc54ab64a7cafd429fe85
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-30 23:41:47 +0530

    Add avatar using random generator

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { TbGridDots } from "react-icons/tb";
+
+import Profile from "../assets/profile-200x200.jpg";
+import ProfileRing from "../assets/profile-ring.svg";
 
 const ProfileIcon = () => {
-  return <div>ProfileIcon</div>;
+  return (
+    <div className="flex gap-2">
+      <span className="h-10 w-10 flex items-center justify-center rounded-full cursor-pointer hover:bg-black/[0.05]">
+        <TbGridDots size={20} color="#5f6368" />
+      </span>
+      <span className="h-10 w-10 relative  flex justify-center items-center">
+        <img src={ProfileRing} alt="profile ring" className="absolute" />
+        <span className="h-8 w-8 rounded-full overflow-hidden">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=female"
+            alt="profile pic"
+            className="h-full w-full object-cover"
+          />
+        </span>
+      </span>
+    </div>
+  );
 };
 
 export default ProfileIcon;
