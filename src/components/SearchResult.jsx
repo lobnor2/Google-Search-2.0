65c3e225a02a1a507cf628557ad97199c07a1b6c
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-06-01 00:46:03 +0530

    Add searchresultheader main footer in search result

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+
+import { fetchDataFromApi } from "../utils/api";
+import SearchResultHeader from "./SearchResultHeader";
+import Footer from "./Footer";
+import SearchedItemTemplate from "./SearchedItemTemplate";
+import SearchedImageItemTemplate from "./SearchedImageItemTemplate";
+import Pagination from "./Pagination";
+import { Context } from "../utils/ContextApi";
 
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  return (
+    <div className="flex flex-col min-h-[100vh]">
+      <SearchResultHeader />
+      <main className="grow p-[12px] pb-0 md:pr-5 md:pl-20"></main>
+      <Footer />
+    </div>
+  );
 };
 
 export default SearchResult;
