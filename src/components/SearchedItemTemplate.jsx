52f32948985407222a9d209a55c3695665beeaa7
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-06-07 23:19:04 +0530

    Add url, title and htmlSnippet using dangerouslySetInnerHTML

--- src/components/SearchedItemTemplate.jsx
+++ src/components/SearchedItemTemplate.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const SearchedItemTemplate = () => {
-  return <div>SearchedItemTemplate</div>;
+const SearchedItemTemplate = ({ key, data }) => {
+  const createMarkup = (html) => {
+    return { __html: html };
+  };
+
+  return (
+    <div className="flex flex-col py-3 max-w-[700px]">
+      <div
+        className="group cursor-pointer "
+        onClick={() => widow.open(data.link)}
+      >
+        <div className="text-sm truncate text-[#202124]">
+          {data.formattedUrl}
+        </div>
+        <div className="text-xl group-hover:underline text-[#1a0dab] pt-2">
+          {data.title}
+        </div>
+      </div>
+      <div
+        className="text-sm text-[#4d5156] leading-6 pt-1"
+        dangerouslySetInnerHTML={createMarkup(data.htmlSnippet)}
+      />
+    </div>
+  );
 };
 
 export default SearchedItemTemplate;
