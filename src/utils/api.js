a68681b57165fe474bf085ce29cfd8b60764ffc1
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-06-10 01:45:48 +0530

    Add api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,13 @@
 import axios from "axios";
 
-export const fetchDataFromApi = () => {};
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+const params = {
+  key: "AIzaSyDe881UTYMQ0osHBOPtJgS4IRgtPDb66Dk",
+  cx: "54baedba80b1c44a5",
+};
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};
